7b9adf9e21d1f1fdb06521e6a9a8796cd4e4a652
Lóránt Pintér <lorant.pinter@gmail.com>
2022-07-27 11:24:19 +0200

    Reproduce problem with passing a c_str() of a String

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,34 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiManager.h>
 #include <esp_sntp.h>
 
 void ntpUpdated(struct timeval* tv) {
     Serial.printf("NTP updated system clock to %ld\n", tv->tv_sec);
 }
 
-void setup() {
+WiFiManager wm;
 
+void setup() {
     Serial.begin(115200);
 
     Serial.printf("Start time is %ld\n", time(nullptr));
 
-    WiFi.begin("...", "...");
-    Serial.print("Connecting...");
-    while (!WiFi.isConnected()) {
-        delay(100);
-        Serial.print(".");
+    // wm.resetSettings();
+    wm.setHostname("sntp-test");
+    wm.setWiFiAutoReconnect(true);
+    if (wm.autoConnect("sntp-test")) {
+        Serial.println("Connected.");
+    } else {
+        Serial.println("Not connected.");
     }
-    Serial.println(" connected.");
 
     sntp_set_time_sync_notification_cb(ntpUpdated);
-    configTime(0, 0, "time.google.com");
+
+    // This works, but passing String.c_str() fails to update:
+    // configTime(0, 0, "time.google.com");
+    String timeServer = "time.google.com";
+    configTime(0, 0, timeServer.c_str());
 }
 
 void loop() {
